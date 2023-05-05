37ff8337102803f4535e07d2c9f55a65c6cde6e2
Riki Wendri <fayyadh.farizzi@gmail.com>
2023-05-05 07:50:15 +0700

    make cleaner code

--- src/components/styled/Destination/Desti.styled.js
+++ src/components/styled/Destination/Desti.styled.js
@@ -42,10 +42,6 @@ export const Destidesc = styled(Vstack)`
         text-transform: uppercase;
         margin:135px 274.5px 0 157.5px;
         transition: all 1s ease-in-out;
-        /* //for animation:
-        animation: expand 1s ease-in-out;
-        animation-direction: alternate;
-        animation-iteration-count: 1; */
     }
     h5{
         font-size: 28px;
